721f3b0aad5f6f01d97e044c7d9bf11b97249105
Ga-yo <39812@naver.com>
2021-02-04 22:05:40 +0900

    [Feat] hello.proj

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,55 @@
 import Vapor
+import Fluent
+import FluentMySQLDriver
 
-// configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
+    let hostname = "127.0.0.1"
+    let username = "root"
+    let password = "baekbaek777"
+    
+    app.databases.use(.mysql(
+                        hostname: hostname,
+                        port: 3306,
+                        username: username,
+                        password: password,
+                        database: "test",
+                        tlsConfiguration: .forClient(certificateVerification: .none)),
+                      as: .mysql)
+    
+    app.migrations.add(CreateGalaxy())
+    
     try routes(app)
+}
+
+
+final class Test: Model, Content {
+    static let schema = "galaxies"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "name")
+    var name: String
+    
+    init() { }
+    
+    init(id: UUID? = nil, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+struct CreateGalaxy: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("galaxies")
+            .id()
+            .field("name", .string)
+            .create()
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("galaxies").delete()
+    }
 }
