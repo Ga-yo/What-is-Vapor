f06b81a10fe5d8f3f4e5d2cdc44412c08d42b66f
Ga-yo <39812@naver.com>
2021-02-05 11:01:24 +0900

    [Add] ResponseStatus init

--- Sources/App/Utility/ResponseJSON.swift
+++ Sources/App/Utility/ResponseJSON.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Vapor
+
+enum ResponseStatus: Int, Content {
+    case ok = 200
+    case error = 400
+    case unknown = 500
+    
+    var desc = String {
+        switch self{
+        case .ok:
+            return "요청이 성공했습니다"
+        case .error:
+            return "요청이 실패했습니다"
+        case .unknown:
+            return "에러의 원인을 모릅니다"
+        }
+    }
+}
+
+struct ResponseJSON<T: Content>: Content {
+    private var status: ResponseStatus
+    private var message: String
+    private var data: T?
+    
+    init(data: T) {
+        self.status = .ok
+        self.message = status.desc
+        self.data = data
+    }
+    
+    init(status: ResponseStatus = .ok) {
+        self.status = status
+        self.message = message
+        self.data = nil
+    }
+    
+    init(status: ResponseStatus = .ok, message: String = ResponseStatus.ok.desc, data: T?) {
+        self.status = status
+        self.message = message
+        self.data = data
+    }
+}
+
+struct Empty: Content { }
