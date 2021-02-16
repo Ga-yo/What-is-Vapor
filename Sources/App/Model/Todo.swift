e3b99a24aaab9de81287623925ecd40ec3bb0485
Ga-yo <39812@naver.com>
2021-02-16 22:46:14 +0900

    ADD study basic routes

---
+++ Sources/App/Model/Todo.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이가영 on 2021/02/16.
+//
+
+import Foundation
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,7 +9,43 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
     
-    app.get("galaxies") { req in
-        return Test.query(on: req.db).all()
+    app.get("name") { (req) in
+        return "Ethan Hunt"
+    }
+
+    //hello/\(name)
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello \(name)!"
+    }
+    
+    // foo/bar/baz
+    app.get("foo", "bar", "baz") {req in
+        return "bar"
+    }
+    
+    //int형
+    app.get("number", ":x") {req -> String in
+        guard let int = req.parameters.get("x", as: Int.self) else {throw Abort(.badRequest)}
+        return "\(int) is a great number"
+    }
+    
+    //hello/foo/bar = foo bar
+    app.get("hello", "**") { (req) -> String in
+        let name = req.parameters.getCatchall().joined(separator: " ")
+        print(app.routes.all)
+        return "Hello \(name)"
+    }
+    
+    //경로 그룹
+    let users = app.grouped("users")
+    
+    users.get { req in
+        return "group users"
+    }
+    
+    users.get(":id") { req -> String in
+        let id = req.parameters.get("id")!
+        return "group users \(id)"
     }
 }
